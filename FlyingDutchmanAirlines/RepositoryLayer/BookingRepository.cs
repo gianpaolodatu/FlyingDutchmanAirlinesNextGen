a92d4908d52bc37441fceea7a8b2c41e8bfc06dd
Gianpaolo <gpaolodatu@gmail.com>
2021-07-31 14:32:47 +0200

    boh

---
+++ FlyingDutchmanAirlines/RepositoryLayer/BookingRepository.cs
@@ -0,0 +1,19 @@
+﻿using FlyingDutchmanAirlines.DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyingDutchmanAirlines.RepositoryLayer
+{
+    public class BookingRepository 
+    {
+        private FlyingDutchmanAirlinesContext _context;
+
+        public BookingRepository(FlyingDutchmanAirlinesContext _context)
+        {
+            this._context = _context;
+        }
+    }
+}
---
+++ FlyingDutchmanAirlines_Tests/RepositoryLayer/BookingRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.RepositoryLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyingDutchmanAirlines_Tests.RepositoryLayer
+{
+    [TestClass]
+    public class BookingRepositoryTests
+    {
+        private FlyingDutchmanAirlinesContext _context;
+        private BookingRepository _repository;
+
+        public void TestInitialize()
+        {
+            DbContextOptions<FlyingDutchmanAirlinesContext> dbContextOptions
+                = new DbContextOptionsBuilder<FlyingDutchmanAirlinesContext>()
+                .UseInMemoryDatabase("FlyingDutchman").Options;
+
+            _context = new FlyingDutchmanAirlinesContext(dbContextOptions);
+
+            _repository = new BookingRepository(_context);
+            Assert.IsNotNull(_repository);
+                            
+        }
+    }
+}
